3a7307855e0c5ecdec0abfa5c5c46c1f5ecf8aa7
Hendrik Schreiber <hs@tagtraum.com>
2021-04-24 15:29:39 +0200

    Extracted duplicate ReaderPump.

--- japlscript-java/src/main/java/com/tagtraum/japlscript/execution/Osascript.java
+++ japlscript-java/src/main/java/com/tagtraum/japlscript/execution/Osascript.java
@@ -92,47 +92,4 @@ public class Osascript extends ScriptExecutor {
         }
         return stdout.getValue();
     }
-
-    private static class ReaderPump implements Runnable {
-
-        private final Reader in;
-        private String value;
-        private IOException ioException;
-        private static final int ONE_KB = 1024;
-
-        public ReaderPump(final Reader in) {
-            this.in = in;
-        }
-
-        public String getValue() {
-            return this.value;
-        }
-
-        public IOException getIOException() {
-            return ioException;
-        }
-
-        @Override
-        public void run() {
-            final char[] cbuf = new char[ONE_KB];
-            final StringBuilder sb = new StringBuilder();
-            int count;
-            try {
-                while ((count = in.read(cbuf)) != -1) {
-                    sb.append(cbuf, 0, count);
-                }
-                this.value = sb.toString().trim();
-            } catch (IOException ioe) {
-                if (LOG.isDebugEnabled()) LOG.debug(ioe.toString(), ioe);
-                this.value = ioe.toString();
-                this.ioException = ioe;
-            } finally {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    if (LOG.isDebugEnabled()) LOG.debug(e.toString(), e);
-                }
-            }
-        }
-    }
 }
