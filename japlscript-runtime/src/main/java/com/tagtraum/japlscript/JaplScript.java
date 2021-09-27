13ba81124b10704e43e103243de8c22abc4c15e4
Hendrik Schreiber <hs@tagtraum.com>
2021-09-27 09:55:57 +0200

    Attempt to avoid java.lang.Record ambiguity.

--- japlscript-runtime/src/main/java/com/tagtraum/japlscript/JaplScript.java
+++ japlscript-runtime/src/main/java/com/tagtraum/japlscript/JaplScript.java
@@ -15,8 +15,9 @@ import com.tagtraum.japlscript.language.Double;
 import com.tagtraum.japlscript.language.Float;
 import com.tagtraum.japlscript.language.Integer;
 import com.tagtraum.japlscript.language.Long;
-import com.tagtraum.japlscript.language.*;
 import com.tagtraum.japlscript.language.Short;
+import com.tagtraum.japlscript.language.Record;
+import com.tagtraum.japlscript.language.*;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
@@ -58,25 +59,25 @@ public final class JaplScript {
 
 
     private static void addDefaultTypes() {
-        addType(Text.getInstance());
-        addType(Integer.getInstance());
-        addType(Short.getInstance());
-        addType(Long.getInstance());
-        addType(Float.getInstance());
-        addType(Double.getInstance());
-        addType(Boolean.getInstance());
-        addType(Date.getInstance());
-        addType(Alias.getInstance());
-        addType(Data.getInstance());
-        addType(Picture.getInstance());
-        addType(Tdta.getInstance());
-        addType(JaplScriptFile.getInstance());
-        addType(Point.getInstance());
-        addType(Rectangle.getInstance());
-        addType(RGBColor.getInstance());
-        addType(TypeClass.getInstance());
-        addType(Record.getInstance());
-        addType(ReferenceImpl.getInstance());
+        addType(com.tagtraum.japlscript.language.Text.getInstance());
+        addType(com.tagtraum.japlscript.language.Integer.getInstance());
+        addType(com.tagtraum.japlscript.language.Short.getInstance());
+        addType(com.tagtraum.japlscript.language.Long.getInstance());
+        addType(com.tagtraum.japlscript.language.Float.getInstance());
+        addType(com.tagtraum.japlscript.language.Double.getInstance());
+        addType(com.tagtraum.japlscript.language.Boolean.getInstance());
+        addType(com.tagtraum.japlscript.language.Date.getInstance());
+        addType(com.tagtraum.japlscript.language.Alias.getInstance());
+        addType(com.tagtraum.japlscript.language.Data.getInstance());
+        addType(com.tagtraum.japlscript.language.Picture.getInstance());
+        addType(com.tagtraum.japlscript.language.Tdta.getInstance());
+        addType(com.tagtraum.japlscript.language.JaplScriptFile.getInstance());
+        addType(com.tagtraum.japlscript.language.Point.getInstance());
+        addType(com.tagtraum.japlscript.language.Rectangle.getInstance());
+        addType(com.tagtraum.japlscript.language.RGBColor.getInstance());
+        addType(com.tagtraum.japlscript.language.TypeClass.getInstance());
+        addType(com.tagtraum.japlscript.language.Record.getInstance());
+        addType(com.tagtraum.japlscript.language.ReferenceImpl.getInstance());
     }
 
     private static void addDefaultGlobalAspects() {
